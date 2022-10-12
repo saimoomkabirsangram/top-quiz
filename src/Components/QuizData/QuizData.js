511cab1bd8b62bc1f519594293b7515cee41f19e
saimoomkabirsangram <saimoomkabirsangram@gmail.com>
2022-10-12 21:11:20 +0600

    done

--- src/Components/QuizData/QuizData.js
+++ src/Components/QuizData/QuizData.js
@@ -1,21 +1,54 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 import QuestionData from '../QuestionData/QuestionData';
 import Quiz from '../Quiz/Quiz';
 
 function QuizData({quizData}) {
     // console.log(quizData);
     const {correctAnswer, id, question} = quizData;
+    
+    const notify = ()=> toast.info(correctAnswer, {
+        position: "top-right",
+        autoClose: 1000 ,
+        closeOnClick: true ,
+        pauseOnHover: true ,
+        draggable: true 
+    })
+    
+    const compareAsnwer = ansData =>{
+        if(ansData === correctAnswer){
+            toast(correctAnswer,{
+                position: "top-right",
+                autoClose: 1000 ,
+                closeOnClick: true ,
+                pauseOnHover: true ,
+                draggable: true 
+            })
+        }
+    }
+    
     return (
-        <div className='bg-gray-200 p-3 rounded-lg'>
+        <div>
+            <div className='bg-gray-200 p-3 rounded-lg'>
             <div> Question : {question}
             </div>
-            {
-                quizData.options.map((option) => <QuestionData
-                    key={option.id}
-                    id={id}
-                    correctAnswer={correctAnswer}
-                    option={option}
-                ></QuestionData>)}
+            <div>
+                {
+                    quizData.options.map((option) => <QuestionData
+                        key={option.id}
+                        id={id}
+                        correctAnswer={correctAnswer}
+                        option={option}
+                    ></QuestionData>)}
+            </div>
+            <div className='mt-3' onClick={() => {compareAsnwer(correctAnswer)}}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+             <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+            </svg>
+
+            </div>
+        </div>
         </div>
     );
 }
